0dcbb2c4f54ccd6f39f0e1a5d4288c021f3c9167
Fabrício Araújo <fabaraujo23@gmail.com>
2021-01-06 16:46:07 -0300

    First commit

---
+++ src/main/java/com/github/fabbaraujo/facadeexample/FacadeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.fabbaraujo.facadeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacadeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacadeExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fabbaraujo/facadeexample/FacadeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabbaraujo.facadeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacadeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
